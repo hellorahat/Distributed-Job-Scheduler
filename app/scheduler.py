596a2d10668c11a07e868470948f95d3a83db260
hellorahat <hellorhat@gmail.com>
2026-01-14 21:47:46 -0500

    added context dependency injection to scheduler

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,21 +1,42 @@
-from typing import TYPE_CHECKING
+from typing import cast, TYPE_CHECKING
 
 from app.utils.time import now_ms
 from app.storage.redis_keys import RedisKeys
 from app.transition import enqueue_job
-from app.redis_client import get_redis
-
+from app.model import SchedulerContext
 
 if TYPE_CHECKING:
     from redis import Redis
 
 
-redis = get_redis()
+def _decide_retry_or_dlq(ctx: SchedulerContext, job_id: str, now: int) -> None:
+    redis = ctx.redis
+    config = ctx.config
+
+    attempts = redis.hget(
+        RedisKeys.JOB.format(job_id),
+        'attempts'
+    )
+
+    if attempts is None:
+        return
+
+    attempts = int(cast(str, attempts))
+
+    if attempts < config.max_retries:
+        # retry
+        delay = now + config.backoff_base_ms * 2 ** (attempts - 1)
+        # enqueue_job()
+    else:
+        # dlq
+        pass
 
 
-def scheduler_tick(redis: 'Redis', limit: int = 100) -> None:
+def scheduler_tick(ctx: SchedulerContext, limit: int = 100) -> None:
+    redis = ctx.redis
     now = now_ms()
 
+    # Decide scheduled -> queued
     job_ids = redis.zrange(
         RedisKeys.JOBS_SCHEDULED,
         start=0,
@@ -24,6 +45,24 @@ def scheduler_tick(redis: 'Redis', limit: int = 100) -> None:
         offset=0,
         num=limit,
     )
+    assert isinstance(job_ids, list)
 
     for job_id in job_ids:
-        enqueue_job(redis, job_id)
+        enqueue_job(ctx, job_id)
+
+    # Decide failed -> retry | failed -> DLQ
+    cursor = 0
+    while True:
+        result = redis.sscan(
+            RedisKeys.JOBS_FAILED,
+            cursor=cursor,
+            count=limit,
+        )
+        assert isinstance(result, tuple)
+
+        cursor, job_ids = result
+        for job_id in job_ids:
+            _decide_retry_or_dlq(ctx, job_id, now)
+
+        if cursor == 0:
+            break
